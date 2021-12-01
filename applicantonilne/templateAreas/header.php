ebc965bffda081a0cf08d3887211b54758b34ded
Espi <joshespi@gmail.com>
2021-12-01 11:09:33 -0700

    added logout function

--- applicantonilne/templateAreas/header.php
+++ applicantonilne/templateAreas/header.php
@@ -5,7 +5,8 @@
   <h1>Applicant Online</h1>
     <nav class="topNav">
       <a href="index.php">Job Search</a>
-  <a href="signup.php">Register to Apply</a>
-  <a href="signin.php">Sign in to Apply</a>
+      <a href="signup.php">Register to Apply</a>
+      <a href="signin.php">Sign in to Apply</a>
+      <a href="logout.php">Sign Out</a>
     </nav>
   </header>
